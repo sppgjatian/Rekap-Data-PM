881a6d6c46c54735861be4a0f9cc41a745a7e2c1
sppgjatian <sppgjatian@gmail.com>
2026-09-23 23:43:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
-   base: '/',
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+})
